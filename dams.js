ced8d796f6bb162210f13a8be5d0dc79affa7e4c
ShukUSWM <159677523+ShukUSWM@users.noreply.github.com>
2024-02-11 16:33:40 +0800

    Add files via upload

---
+++ dams.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "I Love You so much mama ko mwehehee!!!";
+    gif.src = "https://tenor.com/view/kurumin-chan-arigatou-thanks-thank-you-gif-11056320";
+});
+
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.left = randomY + "px";
+}); 
